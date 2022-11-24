be52ddead2506a94df0d6dea62aa71347055e579
Lin Chen <eagletiger1@gmail.com>
2022-11-24 15:27:04 -0500

    Updated t1

--- t1/app.py
+++ t1/app.py
@@ -1,9 +1,9 @@
 import dash
-import dash_html_components as html
+from dash import html
 
 app = dash.Dash(__name__)
 
-app.layout = 'Hello World!'
+app.layout = html.Div('Hello World!')
 
 if __name__ == "__main__":
     app.run_server(debug=True)
