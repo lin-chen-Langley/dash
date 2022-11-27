a776d42c31fa87c3e11f72b6c3a0d947f829bdbf
Lin Chen <eagletiger1@gmail.com>
2022-11-26 19:51:35 -0500

    t2

--- t1/app.py
+++ t1/app.py
@@ -3,7 +3,16 @@ from dash import html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div('Hello World!')
+def get_layout():
+    return html.Div([
+        html.H1('Title'),
+        html.P('Content'),
+        html.Div([
+            html.Li('Item')
+        ])
+    ])
+
+app.layout = get_layout()
 
 if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ t2/app.py
@@ -0,0 +1,29 @@
+import dash
+from dash import html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+def get_c1():
+    return html.Div(html.Button('Click', id='button'))
+
+def get_c2():
+    return html.Div(html.Label('Place Holder', id='output'))
+
+def get_layout():
+    return html.Div([
+        get_c1(),
+        get_c2()
+    ])
+
+app.layout = get_layout()
+
+@app.callback(
+    Output("output", "children"),
+    Input("button", "n_clicks"),
+)
+def update_click(n):
+    return str(n)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
